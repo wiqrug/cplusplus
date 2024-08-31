0127d0e7fc20bd5244c93f10f78c0fdbefd14d70
wiqrug <papavasiliou.alexios@gmail.com>
2024-08-31 21:18:55 +0300

    completed a26 for second time

--- a26/aa26.cpp
+++ a26/aa26.cpp
@@ -1,115 +1,171 @@
+/*
+Να γραφεί κλάση προϊόντων (“Product”) με τα ακόλουθα χαρακτηριστικά:
+Κωδικός (char *)
+Περιγραφή (string)
+Τιμή Μονάδας (float)
+Η κλάση θα παρέχει στον χρήστη της την απαιτούμενη λειτουργικότητα ώστε να
+μπορούν να υλοποιηθούν τα ακόλουθα ζητούμενα:
+Να γραφεί συνάρτηση η οποία θα διαβάζει όλα τα προϊόντα από αρχείο κειμένου με γραμμογράφηση:
+<Κωδικός>;<Περιγραφή>;<Τιμή Μονάδας> (ένα προϊόν ανά γραμμή) και θα τα επιστρέφει σε λίστα
+Να γραφεί συνάρτηση η οποία θα διαγράφει από λίστα προϊόντων όλα τα
+προϊόντα που έχουν συγκεκριμένο κόστος ανά μονάδα.
+Να επιδειχθεί η λειτουργία των παραπάνω μέσω κατάλληλης main. Δεν χρειάζεται να
+εισαχθούν δεδομένα από την standard είσοδο.
+Το πρόγραμμα θα αναπτυχθεί, στο σύνολό του, σε ένα ενιαίο .cpp αρχείο το οποίο θα
+αναρτηθεί στο eclass.
+*/
+
 #include <iostream>
-#include <fstream>
-#include <vector>
 #include <string>
 #include <cstring>
 #include <vector>
 #include <sstream>
+#include <fstream>
 using namespace std;
-
-
 
 class Product{
 	private:
 		char* password;
 		string description;
-		float value;
-
-
+		float price;
 	public:
 		Product();
-		Product(const char*, const string, const float);
-		Product(const Product&);
+		Product(const char*,const string&, const float&);
 		~Product();
-		Product& operator=(const Product&);
+		Product(const Product&);
+		Product& operator= (const Product&);
+		float getPrice()const;
+		 void display()const;
 };
 
 
-vector<Product> readFromFile(const string& filename){
-	vector<Product> products;
-	ifstream str(filename);
+float Product::getPrice()const{return price;}
 
 
-	if (str.is_open()){
-	string line;
-	while(getline(str,line)){
-		stringstream ss(line);
-
-		string password;
-		string description;
-		string value;
-		getline(ss,password,';');
-		getline(ss,description,';');
-		getline(ss,value,';');
-		float v = stof(value);
-		Product product(password.c_str(),description,v);
-		products.push_back(product);
-
+// Function to delete products by price
+vector<Product>& deleteByPrice(vector<Product>& products, float p) {
+	auto it = products.begin();
+	while (it != products.end()) {
+		if (it->getPrice() == p) {
+			it = products.erase(it);  // Remove product and update iterator
+	} else {
+		++it;
+		}
 	}
-	str.close();
-	}
-	else{
-		cerr<<"probably there was an error"<<endl;
-	}
-
 	return products;
 }
 
 
 
 
+vector<Product> products (const string& fname){
+	ifstream ifs(fname);
+	vector<Product> vec;
+	if (ifs.is_open()){
+		string text;
+		while (getline(ifs,text)){
+			stringstream ss(text);
+			string password,description,price;
+			getline(ss,password,';');
+			getline(ss,description,';');
+			getline(ss,price,';');
 
-Product& Product::operator=(const Product& other){
-	if (this==&other){return *this;}
+			int intPrice = stof(price);
+			Product p(password.c_str(),description,intPrice);
+			vec.push_back(p);
+		}
+	}
+	else{cerr<<"Error opening file"<<endl;}
+return vec;
+}
 
-	delete[] password;
-	description = other.description;
-	value = other.value;
-	password = new char[strlen(other.password)+1];
-	strcpy(password,other.password);
-	return *this;
+
+int main () {
+//testing empty constructor
+
+Product emptyConstructor;
+//testing argument constructor
+Product argConstructor("iPhone","telephone",500);
+
+//testing copy constructor
+Product copyConstructor(argConstructor);
+
+
+//testing = overload
+emptyConstructor=copyConstructor;
+
+
+//testing vector function
+vector<Product> vec = products("products.txt");
+
+for (const auto& item:vec){item.display();}
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
+
+
+
+
+void Product::display() const{
+	cout<<"Password: "<<password<<" | ";
+	cout<<"Description: "<<description<<" | ";
+	cout<<"Price: "<<price<<endl;
+	cout<<"----------------------------------------------"<<endl;
 }
 
 
 
 Product::Product(){
 	description="";
-	value=0;
-
+	price=0;
 	password = new char[1];
 	password[0] = '\0';
-
 }
 
-Product::Product(const char* p, const string d, const float v){
-	description = d;
-	value = v;
+Product::Product(const char* pass,const string& desc,const float& pr){
+		description = desc;
+		price = pr;
+		if (pass){
+			password = new char[strlen(pass)+1];
+			strcpy(password,pass);
+		}
+		else{
+			password = new char[1];
+			password[0] = '\0';
+		}
 
-	if (p){
-		password = new char[strlen(p)+1];
-		strcpy(password,p);
-	}
-	else{
-		password = new char[1];
-		password[0] = '\0';
-	}
-
-}
-
-
-Product::Product(const Product& other){
-	description = other.description;
-	value=other.value;
-	password = new char[strlen(other.password)+1];
-	strcpy(password,other.password);
 }
 
 
 Product::~Product(){delete[] password;}
 
-int main () {
-cout<<"Hello friends"<<endl;
+Product& Product::operator= (const Product& other){
+	if (this == &other){return *this;}
 
-return 0;
+	delete[] password;
+	password = new char[strlen(other.password)+1];
+	strcpy(password,other.password);
 
+	description = other.description;
+	price = other.price;
+
+return *this;
+}
+
+
+Product::Product(const Product& other){
+	description= other.description;
+	price= other.price;
+
+	password = new char[strlen(other.password)+1];
+	strcpy(password,other.password);
 }
